f0cb832bc176bfae139397cfd7408b8e3a0147f4
MarioMenjivar11 <61217700+MarioMenjivar11@users.noreply.github.com>
2020-02-23 22:24:35 -0600

    added the file that contains the routes of movies

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,5 +1,22 @@
 import { Router } from 'express';
 const router = Router();
 
+import { create_movie, get_movies, get_one_movie, delete_movie, update_movies } from '../controllers/movies.controller'
+
+//api/movies
+router.post('/', create_movie );
+
+//api/movies/get
+router.get('/', get_movies);
+
+//api/movies/{id}
+router.get('/:id', get_one_movie);
+
+//api/movies/delete/{id}
+router.delete('/:id', delete_movie);
+
+//api/movies/put/{id}
+router.put('/:id', update_movies);
+
 export default router;
 
